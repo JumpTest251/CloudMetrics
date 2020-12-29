01220e637bcb070bcdd0a7eed7a309cd4ac0e97c
Jumper251 <a.a.m.2502@googlemail.com>
2020-12-29 15:01:58 +0100

    report

--- collectMetrics.js
+++ collectMetrics.js
@@ -3,13 +3,13 @@ const { cpu } = require('node-os-utils');
 
 const { MinecraftServer } = require('mcping-js');
 const { serverPort } = require('./utils/config');
+const reportMetrics = require('./utils/reportMetrics');
 
 function pingServer() {
     return new Promise(resolve => {
         const server = new MinecraftServer('amtest.jumper251.mcservers.me', serverPort);
 
-        server.ping(20000, '47', (err, data) => {
-            console.log(err, ": " + data)
+        server.ping(3000, '47', (err, data) => {
             if (err || !data) return resolve();
 
             resolve(data);
@@ -26,6 +26,12 @@ module.exports.collectAndReport = async function () {
     const cpuUsage = await cpu.usage();
     const players = await pingServer();
     const memory = memoryUsage();
-    console.log(`mem: ${os.freemem()}/${os.totalmem()}`)
     console.log(`cpu: ${cpuUsage}, players: ${players ? players.players.online : ''}, memory: ${memory}`)
+
+    const metrics = [];
+    metrics.push({ type: 'cpu', value: cpuUsage }, { type: 'memory', value: memory });
+
+    if (players) metrics.push({ type: 'players', value: players.players.online });
+
+    reportMetrics(metrics);
 }
